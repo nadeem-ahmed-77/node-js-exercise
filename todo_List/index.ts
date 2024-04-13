28ec38a515e2af1bb1a65c9c66f1843c773d2c95
nadeem-ahmed-77 <165781212+nadeem-ahmed-77@users.noreply.github.com>
2024-04-14 01:16:17 +0500

    Add files via upload

---
+++ todo_List/index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todoList = [];
+let condition = true
+
+while(condition){
+const todoQuestion = await inquirer.prompt(
+    [
+        {
+            name :"item", 
+            type :"input",        
+            message:"What would you like to add in your list?",
+        },   
+        {
+            name :"items", 
+            type :"confirm",        
+            message:"would you like to more add in your list?",
+            default: true
+        }
+    ]
+);
+
+todoList.push(todoQuestion.item)
+console.log(todoList)
+condition = todoQuestion.items 
+};
